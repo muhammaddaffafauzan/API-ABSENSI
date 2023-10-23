ff6fd38733f6f254b93c89f3b9f677a9395d4fd1
muhammaddaffafauzan <mobileasia215@gmail.com>
2023-10-23 16:03:20 +0700

    url

--- config/Database.js
+++ config/Database.js
@@ -2,12 +2,6 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-const db = new Sequelize( process.env.MYSQL_URL,{
-    // username: process.env.DB_USERNAME,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_NAME,
-    // host: process.env.DB_HOST,
-    dialect: 'mysql',
-});
+const db = new Sequelize(process.env.MYSQL_URL);
 
 export default db;
