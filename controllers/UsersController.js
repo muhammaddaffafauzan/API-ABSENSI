b301cb8a8e280e9d5599248112b7db31778b68ba
muhammaddaffafauzan <mobileasia215@gmail.com>
2023-12-03 15:56:10 +0700

    final

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,178 +1,216 @@
 import User from "../models/UsersModel.js";
 import bcryptjs from "bcryptjs";
+// import { v4 as uuidv4 } from "uuid";
 
-
-export const getUsers = async(req, res) => {
-    try {
-            const response = await User.findAll({
-            attributes: ['id','uuid', 'name', 'email', 'role']
-        });
-        res.status(200).json(response);
-    } catch (error) {
-        res.status(500).json({msg: error.message});
-    }
-}
-export const getUsersById = async(req, res) => {
-    try {
-        const response = await User.findOne({
-            attributes: ['uuid', 'name', 'email', 'role'],
-              where: {
-                uuid: req.params.uuid
-              }
-        });
-        res.status(200).json(response);
-    } catch (error) {
-        res.status(500).json({msg: error.message});
-    }
-}
-export const createUsers = async(req, res) => {
-    const {name, email, password } = req.body;
-    const salt = await bcryptjs.genSalt();
-    const hashPassword = await bcryptjs.hash(password, salt);
-    try {
-        await User.create({
-            name: name, 
-            email: email,
-            password: hashPassword,
-            role: 'admin'
-        });
-        res.status(201).json({msg: "register berhasil"})
-    } catch (error) {
-        res.status(400).json({msg: error.message});
-    }
-}
-export const updateUsers = async(req, res) => {
-    const user = await User.findOne({
-          where: {
-            uuid: req.params.uuid
-          }
-    });
-    if(!user) return res.status(404).json({msg: "user tidak ditemukan"});
-    const {name, email, password, confPassword, role} = req.body;
-    let hashPassword;
-    if (password === "" || password === null) {
-        hashPassword = user.password
-    }else{
-        const salt = await bcryptjs.genSalt();
-        hashPassword = await bcryptjs.hash(password, salt);
-    }
-    if(password !== confPassword) return res.status(400).json({msg: "Password dan ConfirmPassword Tidak Cocok"});
-    try {
-        await User.update({
-            name: name,
-            email: email,
-            password: hashPassword,
-            role: role
-        },{
-            where: {
-               id: user.id
-            }
-        });
-        res.status(200).json({msg: "Users Updated"})
-    } catch (error) {
-        res.status(400).json({msg: error.message});
-    }
-}
-
-export const updatePassUser = async(req, res) => {
-        const user = await User.findOne({
-          where: {
-            id: req.userId
-          }
-    });
-
-    if(!user) return res.status(404).json({msg: "user tidak ditemukan"});
-    const { password, confPassword,} = req.body;
-    let hashPassword;
-    if (password === "" || password === null) {
-        hashPassword = user.pas
-    }else{
-        const salt = await bcryptjs.genSalt();
-        hashPassword = await bcryptjs.hash(password, salt);
-    }
-    if(password !== confPassword) return res.status(400).json({msg: "Password dan ConfirmPassword Tidak Cocok"});
-    try {
-        if(req.role === 'user')
-       { 
-        await User.update({
-            name: user.name,
-            email: user.email,
-            password: hashPassword,
-            role: 'user'
-        },{
-            where: {
-               id: user.id
-            }
-        });
-    }else{
-        res.status(400).json({msg:"ini untuk user mengubah password"});
-    }
-        res.status(200).json({msg: "Users Updated"})
-    } catch (error) {
-        res.status(400).json({msg: error.message});
-    }
-}
-
-export const requestPasswordReset = async (req, res) => {
-    const { email } = req.body;
-    const user = await User.findOne({ where: { email } });
-
-    if (!user) {
-        return res.status(404).json({ msg: "Pengguna tidak ditemukan" });
-    }
-
-    const resetToken = crypto.randomBytes(20).toString('hex');
-    // Atur validitas token (misalnya, 1 jam)
-    const resetTokenExpires = Date.now() + 3600000;
-
-    await User.update({ resetToken, resetTokenExpires }, { where: { email } });
-
-    // Kirim email dengan resetToken (Implementasikan logika nodemailer atau layanan email lainnya di sini)
-
-    res.status(200).json({ msg: "Email reset password telah dikirim" });
-};
-
-export const resetPassword = async (req, res) => {
-    const { resetToken, newPassword } = req.body;
-    const user = await User.findOne({
-        where: {
-            resetToken,
-            resetTokenExpires: {
-                [Op.gt]: Date.now() // Op.gt adalah operator 'greater than'
-            }
-        }
-    });
-
-    if (!user) {
-        return res.status(400).json({ msg: "Token tidak valid atau telah kadaluwarsa" });
-    }
-
-    const salt = await bcryptjs.genSalt();
-    const hashedPassword = await bcryptjs.hash(newPassword, salt);
-
-    await User.update({ password: hashedPassword, resetToken: null, resetTokenExpires: null }, {
-        where: { id: user.id }
-    });
-
-    res.status(200).json({ msg: "Password telah diperbarui" });
-};
-
-
-export const deleteUsers = async(req, res) => {
-    const user = await User.findOne({
-        where: {
-          uuid: req.params.uuid
-        }
-  });
-  if(!user) return res.status(404).json({msg: "user tidak ditemukan"});
+export const getUsers = async (req, res) => {
   try {
-      await User.destroy({
-          where: {
-             id: user.id
-          }
-      });
-      res.status(200).json({msg: "Users deleted"})
+    const response = await User.findAll({
+      attributes: ["id", "uuid", "name", "email", "role"],
+    });
+    res.status(200).json(response);
   } catch (error) {
-      res.status(400).json({msg: error.message});
+    res.status(500).json({ msg: error.message });
   }
-}
+};
+export const getUsersById = async (req, res) => {
+  try {
+    const response = await User.findOne({
+      attributes: ["uuid", "name", "email", "role"],
+      where: {
+        uuid: req.params.uuid,
+      },
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+export const createUsers = async (req, res) => {
+  const { name, email, password } = req.body;
+  const salt = await bcryptjs.genSalt();
+  const hashPassword = await bcryptjs.hash(password, salt);
+  try {
+    await User.create({
+      name: name,
+      email: email,
+      password: hashPassword,
+      role: "admin",
+    });
+    res.status(201).json({ msg: "register berhasil" });
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+};
+export const updateUsers = async (req, res) => {
+  const user = await User.findOne({
+    where: {
+      uuid: req.params.uuid,
+    },
+  });
+  if (!user) return res.status(404).json({ msg: "user tidak ditemukan" });
+  const { name, email, password, confPassword, role } = req.body;
+  let hashPassword;
+  if (password === "" || password === null) {
+    hashPassword = user.password;
+  } else {
+    const salt = await bcryptjs.genSalt();
+    hashPassword = await bcryptjs.hash(password, salt);
+  }
+  if (password !== confPassword)
+    return res
+      .status(400)
+      .json({ msg: "Password dan ConfirmPassword Tidak Cocok" });
+  try {
+    await User.update(
+      {
+        name: name,
+        email: email,
+        password: hashPassword,
+        role: role,
+      },
+      {
+        where: {
+          id: user.id,
+        },
+      }
+    );
+    res.status(200).json({ msg: "Users Updated" });
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+};
+
+export const updatePassUser = async (req, res) => {
+  const user = await User.findOne({
+    where: {
+      id: req.userId,
+    },
+  });
+
+  if (!user) return res.status(404).json({ msg: "user tidak ditemukan" });
+  const { password, confPassword } = req.body;
+  let hashPassword;
+  if (password === "" || password === null) {
+    hashPassword = user.pas;
+  } else {
+    const salt = await bcryptjs.genSalt();
+    hashPassword = await bcryptjs.hash(password, salt);
+  }
+  if (password !== confPassword)
+    return res
+      .status(400)
+      .json({ msg: "Password dan ConfirmPassword Tidak Cocok" });
+  try {
+    if (req.role === "user") {
+      await User.update(
+        {
+          name: user.name,
+          email: user.email,
+          password: hashPassword,
+          role: "user",
+        },
+        {
+          where: {
+            id: user.id,
+          },
+        }
+      );
+    } else {
+      res.status(400).json({ msg: "ini untuk user mengubah password" });
+    }
+    res.status(200).json({ msg: "Users Updated" });
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+};
+
+// export const resetPassword = async (req, res) => {
+//   const { email } = req.body;
+
+//   try {
+//     const user = await User.findOne({
+//       where: {
+//         email: email,
+//       },
+//     });
+
+//     if (!user) {
+//       return res.status(404).json({ msg: "User not found" });
+//     }
+
+//     // Generate tautan unik untuk reset password
+//     const resetLink = uuidv4();
+
+//     await User.update(
+//       { resetPasswordToken: resetLink },
+//       {
+//         where: {
+//           id: user.id,
+//         },
+//       }
+//     );
+
+//     // Kirim email reset password (gunakan fungsi yang sesuai)
+//     // contoh: sendResetPasswordEmail(user.email, resetLink);
+
+//     res.status(200).json({ msg: "Reset password link sent to your email" });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ msg: "Internal Server Error" });
+//   }
+// };
+
+// export const changePasswordWithoutToken = async (req, res) => {
+//   const { resetLink, newPassword } = req.body;
+
+//   try {
+//     const user = await User.findOne({
+//       where: {
+//         resetPasswordToken: resetLink,
+//       },
+//     });
+
+//     if (!user) {
+//       return res.status(404).json({ msg: "Invalid reset link" });
+//     }
+
+//     const salt = await bcryptjs.genSalt();
+//     const hashPassword = await bcryptjs.hash(newPassword, salt);
+
+//     await User.update(
+//       {
+//         password: hashPassword,
+//         resetPasswordToken: null,
+//       },
+//       {
+//         where: {
+//           id: user.id,
+//         },
+//       }
+//     );
+
+//     res.status(200).json({ msg: "Password updated successfully" });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ msg: "Internal Server Error" });
+//   }
+// };
+
+export const deleteUsers = async (req, res) => {
+  const user = await User.findOne({
+    where: {
+      uuid: req.params.uuid,
+    },
+  });
+  if (!user) return res.status(404).json({ msg: "user tidak ditemukan" });
+  try {
+    await User.destroy({
+      where: {
+        id: user.id,
+      },
+    });
+    res.status(200).json({ msg: "Users deleted" });
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+};
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -6,21 +6,20 @@ import {
   updateUsers,
   updatePassUser,
   deleteUsers,
-  requestPasswordReset,
-  resetPassword
+  // resetPassword,
+  // changePasswordWithoutToken,
 } from "../controllers/UsersController.js";
 import { verifyUser, adminOnly } from "../middleware/AuthUser.js";
 
 const router = express.Router();
 
-// Rute yang sudah ada
 router.get('/api/v1/users', adminOnly, verifyUser,  getUsers);
 router.get('/api/v1/users/:uuid', adminOnly, verifyUser,  getUsersById);
 router.post('/api/v1/users/add', adminOnly, verifyUser, createUsers);
 router.patch('/api/v1/users/update/:uuid', adminOnly, verifyUser, updateUsers);
 router.patch('/api/v1/users/updatePass', verifyUser, updatePassUser);
 router.delete('/api/v1/users/destroy/:uuid', adminOnly, verifyUser, deleteUsers);
-router.post('/api/v1/users/requestPasswordReset', requestPasswordReset);
-router.post('/api/v1/users/resetPassword', resetPassword);
+// router.post('/api/v1/auth/reset-password', resetPassword);
+// router.post('/api/v1/auth/change-password', changePasswordWithoutToken);
 
 export default router;
