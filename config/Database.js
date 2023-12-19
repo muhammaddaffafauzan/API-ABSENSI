057a2cc2ed71159db6d62167ecefa123c5db0560
muhammaddaffafauzan <mobileasia215@gmail.com>
2023-12-19 21:12:19 +0700

    config data base

--- config/Database.js
+++ config/Database.js
@@ -1,12 +1,16 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
-import mysql2 from "mysql2"
+import mysql2 from "mysql2";
 
 dotenv.config();
 
-const db = new Sequelize(process.env.MYSQL_URL,{
-    "dialect" : "mysql",
-    "dialectModule" : mysql2
+const db = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  dialect: "mysql",
+  dialectModule: mysql2,
 });
 
 export default db;
